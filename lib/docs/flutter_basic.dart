98c9f3fe8b980b33623dd43ad5fe698e8e49bd95
sumitmishra <mishra.sumit@thinksys.com>
2024-07-16 10:10:48 +0530

    added flutter basics theory

---
+++ lib/docs/flutter_basic.dart
@@ -0,0 +1,65 @@
+// What is Flutter?
+// A tool that allows you to build native cross-platform (Android , IoS, Web, Deskstop)
+// applications with one programming language and cosebase
+
+// Cross Platform is also known as multiplatform or platform independent.
+
+// Native vs CrossPlatform vs Hybrid
+
+// Native : An application developed exclusively 
+// for a single platform using a native-to-the-operating-system language is a Native App.
+
+// Hybrid :
+// A blend solution that consists of both native and web solutions. 
+// Developers embed the code written with web development languages (HTML, CSS and JS) 
+// into a native app using plugins like Apache Cordova or Ionic's Capacitor.
+
+// Cross Platform :
+// Write code once and reuse for different platforms.
+
+
+// What is flutter?
+// Flutter is a cross-platform mobile application development framework.
+
+// Why flutter?
+// It greatly increases development speed while reducing costs - 
+// two of the most important factors when creating any app.
+// In addition, it's built-in widgets allow developers
+// to quickly build custom user interfaces without writing complex 
+//codes from scratch or manually adjusting for different platforms.
+
+
+// Benefits of Flutter?
+// 1. Same UI and Business Logics in all platforms 
+// 2. Reduced Code Development Time
+// 3. Increased Time-to-market speed 
+// 4. Similar to Native App Performance
+// 5. Custom, Animated UI of any complexity available
+// 6. Own Rendering Engine
+// 7. Simple Platform Specific Logic Implementation
+// 8. The Potential ability to go beyond mobile
+// 9. Large Community Support
+// 10. Continous sdk updates in flutter
+// 11. Provide AOT(A Head of Time) and JIT (Just in time) features
+
+
+// How it is better than it's counterparts? How it is better than others?
+// For ex: - 
+// In react native when we used RaisedButton so when this run on ios then ios called its native button UI Button so here it is creating a bridge that's why some time is taken in this process and same for android android called it's native button Widget.Button.
+// But in flutter when we used RaisedButton so "It directly controls every pixel which is drawn" (Custom Implementation)
+// So here it is not creating any bridge so it is fast and efficient.
+
+// In React Native there are two main components 
+// 1. Platform :- By which user can interact with the app like  Canvas and Events -> OEM Widgets
+// 2. Services :- (Location, Bluetooth, Audio, Sensors, Camera, etc.)
+//  so for use Services on user interaction bridges are created 
+
+
+// In flutter there are also two main components 
+// 1. Platform :- By which user can interact with the app like  Canvas and Events -> Widgets Rendering
+// 2. Services :- (Location, Bluetooth, Audio, Sensors, Camera, etc.) -> Platform Channels
+//  so for use Services on user interaction Platform Channels are used (No bridges are required)
+
+// In flutter everything is Widgets
+// It's all about 
+
